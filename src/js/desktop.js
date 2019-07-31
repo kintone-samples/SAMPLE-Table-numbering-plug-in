eccc0b1371c25bcd595c67f3b2f91393ceb6af2d
Niki <43398321+niki-cy@users.noreply.github.com>
2019-07-31 14:54:07 +0900

    Edited indent spacing from 4 to 2 spaces

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -5,51 +5,51 @@
  * Licensed under the MIT License
  */
 (function(PLUGIN_ID) {
-    'use strict';
+  'use strict';
 
-    // Get plug-in configuration settings
-    var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
-    // Get each setting
-    if (!CONFIG) {
-        return false;
+  // Get plug-in configuration settings
+  var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+  // Get each setting
+  if (!CONFIG) {
+    return false;
+  }
+
+  var TABLEFIELD = CONFIG.table_field; // Field code of the table
+  var NUMBERFIELD = CONFIG.number_field; // Field code of number field in the table
+
+  // Disable number fields in table at these events
+  var disableEvents = [
+    'app.record.edit.show',
+    'app.record.create.show',
+    'app.record.edit.change.' + TABLEFIELD,
+    'app.record.create.change.' + TABLEFIELD
+  ];
+
+  kintone.events.on(disableEvents, function(event) {
+    var record = event.record;
+
+    // Disable number fields in table rows
+    var count = record[TABLEFIELD].value.length;
+    for (var i = 0; i < count; i++) {
+      record[TABLEFIELD].value[i].value[NUMBERFIELD].disabled = true;
+    }
+    return event;
+  });
+
+  // Number table rows at these events
+  var numberEvents = [
+    'app.record.create.submit',
+    'app.record.edit.submit'
+  ];
+  kintone.events.on(numberEvents, function(event) {
+    var record = event.record;
+
+    // Auto-number the table rows
+    var count = record[TABLEFIELD].value.length;
+    for (var i = 0; i < count; i++) {
+      record[TABLEFIELD].value[i].value[NUMBERFIELD].value = i + 1;
     }
 
-    var TABLEFIELD = CONFIG.table_field; // Field code of the table
-    var NUMBERFIELD = CONFIG.number_field; // Field code of number field in the table
-
-    // Disable number fields in table at these events
-    var disableEvents = [
-        'app.record.edit.show',
-        'app.record.create.show',
-        'app.record.edit.change.' + TABLEFIELD,
-        'app.record.create.change.' + TABLEFIELD
-    ];
-    
-    kintone.events.on(disableEvents, function(event){
-        var record = event.record;
-
-        // Disable number fields in table rows
-        var count = record[TABLEFIELD].value.length;
-        for (var i = 0; i < count; i++) {
-            record[TABLEFIELD].value[i].value[NUMBERFIELD]['disabled'] = true;
-        };
-        return event;
-    });
-
-    // Number table rows at these events
-    var numberEvents = [
-        'app.record.create.submit',
-        'app.record.edit.submit'
-    ];
-    kintone.events.on(numberEvents, function(event) {
-        var record = event.record;
-
-        // Auto-number the table rows
-        var count = record[TABLEFIELD].value.length;
-        for (var i = 0; i < count; i++) {
-            record[TABLEFIELD].value[i].value[NUMBERFIELD].value = i + 1;
-        }
-
-        return event;
-    });
+    return event;
+  });
 })(kintone.$PLUGIN_ID);
