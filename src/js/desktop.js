1f984c3f792a4c103a2e712cb3989168950a23ba
Niki <43398321+niki-cy@users.noreply.github.com>
2019-08-09 17:40:04 +0900

    Update desktop.js

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -8,29 +8,29 @@
   'use strict';
 
   var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID); // Get plug-in configuration settings
-  var TABLEFIELD = CONFIG.table_field; // Field code of the table
-  var NUMBERFIELD = CONFIG.number_field; // Field code of number field in the table
+  var TABLEFIELD, NUMBERFIELD, disableEvents;
   // Disable number fields in table at these events
-  var disableEvents = [
-    'app.record.edit.show',
-    'app.record.create.show',
-    'app.record.edit.change.' + TABLEFIELD,
-    'app.record.create.change.' + TABLEFIELD
-  ];
-  var numberEvents;
-  var i;
+  var numberEvents, row;
   // Get each setting
   if (!CONFIG) {
     return false;
   }
 
+  TABLEFIELD = CONFIG.table; // Field code of the table
+  NUMBERFIELD = CONFIG.number; // Field code of number field in the table
+  disableEvents = [
+    'app.record.edit.show',
+    'app.record.create.show',
+    'app.record.edit.change.' + TABLEFIELD,
+    'app.record.create.change.' + TABLEFIELD
+  ];
+
   kintone.events.on(disableEvents, function(event) {
     var record = event.record;
-
     // Disable number fields in table rows
     var count = record[TABLEFIELD].value.length;
-    for (i = 0; i < count; i++) {
-      record[TABLEFIELD].value[i].value[NUMBERFIELD].disabled = true;
+    for (row = 0; row < count; row++) {
+      record[TABLEFIELD].value[row].value[NUMBERFIELD].disabled = true;
     }
     return event;
   });
@@ -45,8 +45,8 @@
 
     // Auto-number the table rows
     var count = record[TABLEFIELD].value.length;
-    for (i = 0; i < count; i++) {
-      record[TABLEFIELD].value[i].value[NUMBERFIELD].value = i + 1;
+    for (row = 0; row < count; row++) {
+      record[TABLEFIELD].value[row].value[NUMBERFIELD].value = row + 1;
     }
 
     return event;
